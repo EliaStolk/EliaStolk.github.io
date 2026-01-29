96f86a616739b3afcef61e3d1a47e6cf8a232a11
EliaStolk <esto675@aucklanduni.ac.nz>
2026-01-29 20:14:51 +1300

    added cursor sparkle

--- script.js
+++ script.js
@@ -26,3 +26,28 @@ filterButtons.forEach(btn => btn.classList.remove("active"));
 button.classList.add("active");
 });
 });
+
+document.addEventListener("mousemove", function(e) {
+const sparkle = document.createElement("div");
+sparkle.className = "sparkle";
+document.body.appendChild(sparkle);
+
+
+
+sparkle.style.left = e.clientX + "px";
+sparkle.style.top = e.clientY + "px";
+
+
+
+setTimeout(() => {
+sparkle.style.transition = "transform 0.5s ease, opacity 0.5s ease";
+sparkle.style.transform = `translate(${Math.random()*30-15}px, ${Math.random()*30-15}px)`;
+sparkle.style.opacity = 0;
+}, 0);
+
+
+
+setTimeout(() => {
+sparkle.remove();
+}, 500);
+});

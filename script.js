4f8f84e5771fcb82d41599b6a730fee1ae2d83fa
EliaStolk <esto675@aucklanduni.ac.nz>
2026-02-09 16:35:03 +1300

    added base outline for education page content, trying to animate project cards being filtered

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
+const projectList = document.querySelectorAll(".project-card");
+
 function filterSelection(catagory) {
-    const projectList = document.querySelectorAll(".project-card");
+    
+    projectList.forEach(project => {
+            project.style.opacity = 0;
+        })
     if(catagory === 'all') {
         projectList.forEach(project => {
             project.style.display = 'block';
+            project.style.opacity = 1;
         })
     } else {
         projectList.forEach(project => {
             if(project.classList.contains(catagory)) {
                 project.style.display = 'block';
+                project.style.opacity = 1;
             } else {
                 project.style.display = 'none';
             }
         })
     }
 }
+
+projectList.forEach(project => {
+    project.addEventListener("animationend", project => {
+        
+    })
+})
+

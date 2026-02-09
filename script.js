f8fad608776bd6dadd99d9b4a47e53555d2e4476
EliaStolk <esto675@aucklanduni.ac.nz>
2026-02-09 15:24:45 +1300

    to reaggange filter bar, had to change display not visability

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ function filterSelection(catagory) {
     const projectList = document.querySelectorAll(".project-card");
     if(catagory === 'all') {
         projectList.forEach(project => {
-            project.style.visibility = 'visible';
+            project.style.display = 'block';
         })
     } else {
         projectList.forEach(project => {
             if(project.classList.contains(catagory)) {
-                project.style.visibility = 'visible';
+                project.style.display = 'block';
             } else {
-                project.style.visibility = 'hidden';
+                project.style.display = 'none';
             }
         })
     }
